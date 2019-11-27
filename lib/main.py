f0647964bb2f73a5f2f07ccd3bfe20953a1176ca
William Chrapcynski <wchrapcynski@gmail.com>
2019-11-27 14:15:07 -0500

    Formatted my text better

--- lib/main.py
+++ lib/main.py
@@ -20,19 +20,25 @@ def setup():
     satan = Contact(name='Satan', phone='703-555-1212', address='666 Hell RD, Death Valley, California')
     satan.save()
     print("Seeding contacts to start with.")
+
+def header():
+    print("\033[1;32;40mName\t\tPhone Number\t\tAddress\033[1;37;40m")
     
 def show_menu():
-    menu = input("Please choose one of the follow:\n0. Seed Data (deletes previous data)\n1. Show Contact List\n2. Find Contact by First Name\n3. Add a new contact\n4. Exit\n")
+    menu = input("\nPlease choose one of the follow:\n0. Seed Data (deletes previous data)\n1. Show Contact List\n2. Find Contact by First Name\n3. Add a new contact\n4. Exit\n")
+    print("")
     if menu == "0":
         setup()
         show_menu()
     elif menu == "1":
+        header()
         data = Contact.select()
         for contact in data:
             print(f"{contact.name}\t\t{contact.phone}\t\t{contact.address}")
         show_menu()
     elif menu == "2":
         first_name = input("What is the first name of the person you're looking for?\n")
+        header()
         data = Contact.select().where(Contact.name % f"%{first_name}%") # Can search for part of word
         for contact in data:
             print(f"{contact.name}\t\t{contact.phone}\t\t{contact.address}")
